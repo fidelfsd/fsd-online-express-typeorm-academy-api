62609fac9021f43d73fe4e797b7fc9b33feef018
Fidel <f.gilart@geekshubs.com>
2023-12-14 17:38:16 +0100

    add comments to BaseFactory

--- src/database/factories/BaseFactory.ts
+++ src/database/factories/BaseFactory.ts
@@ -1,26 +1,60 @@
 import { ObjectLiteral, Repository } from "typeorm";
 
-// -----------------------------------------------------------------------------
-
+/**
+ * La clase `BaseFactory` proporciona métodos básicos para generar instancias de entidades
+ * utilizando un repositorio de TypeORM.
+ *
+ * @template T Tipo genérico que representa la entidad.
+ */
 export class BaseFactory<T extends ObjectLiteral> {
+   /**
+    * Repositorio TypeORM utilizado para la creación de instancias.
+    * @protected
+    */
    protected modelRepository: Repository<T>;
 
+   /**
+    * Constructor de la clase.
+    *
+    * @param {Repository<T>} modelRepository Repositorio TypeORM utilizado para la creación de instancias.
+    */
    constructor(modelRepository: Repository<T>) {
       this.modelRepository = modelRepository;
    }
 
+   /**
+    * Método protegido que genera propiedades específicas para una instancia del modelo.
+    * Debe ser sobrescrito por las clases hijas según las necesidades específicas.
+    *
+    * @param {T} model Instancia del modelo a la que se le generarán propiedades específicas.
+    * @returns {T} Instancia del modelo con propiedades específicas generadas.
+    * @protected
+    */
    protected generateSpecifics(model: T): T {
-      /// se sobreescribe en la clase hija
+      // Se sobreescribe en la clase hija según las necesidades específicas.
       return model;
    }
 
+   /**
+    * Método protegido que genera una instancia del modelo con propiedades específicas.
+    * Utiliza el método generateSpecifics que puede ser sobrescrito por clases hijas.
+    *
+    * @returns {T} Instancia del modelo con propiedades específicas generadas.
+    * @protected
+    */
    protected generate(): T {
       let model = this.modelRepository.create();
       model = this.generateSpecifics(model);
       return model;
    }
 
-   createMany(count = 1): T[] {
+   /**
+    * Método público que crea un número especificado de instancias del modelo.
+    *
+    * @param {number} [count=1] Número de instancias a crear (por defecto es 1).
+    * @returns {T[]} Un array de instancias del modelo creadas.
+    */
+   createMany(count: number = 1): T[] {
       const generated: T[] = [];
 
       for (let i = 0; i < count; i++) {
